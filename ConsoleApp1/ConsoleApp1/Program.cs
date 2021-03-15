c5bf9fd1b96dd02a55c085850995980f8b413675
Asencio Ortiz <ortiz19920@iesmarenostrum.com>
2021-03-15 21:57:10 +0100

    primer commit en ramausuario

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("examen!");
             Console.WriteLine("clonado");
+            Console.WriteLine("cambios en ramausuario");
         }
     }
 }
